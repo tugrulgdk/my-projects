2c872a85bbf3cc33ce79c74c99f39a99c17e7435
Tugrul Gedik <109855521+tugrulgdk@users.noreply.github.com>
2022-09-07 19:13:23 +0300

    Add files via upload

---
+++ flask-01-hello-world-app/hello-world-app.py
@@ -0,0 +1,23 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    redgsdgurn "Hello World from Flask"
+
+sdgsdg
+
+sdgf
+""
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
+
+    
+    
+    
+    
